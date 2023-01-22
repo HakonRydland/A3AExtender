5f1bfa416e694a068650dc24337c97ed4c3f6473
Håkon <hrr1@hotmail.no>
2023-01-22 15:57:19 +0100

    updated config.cpp's to use prefix macro to avoid issues for users

--- A3AE/addons/templates/config.cpp
+++ A3AE/addons/templates/config.cpp
@@ -6,7 +6,7 @@ class CfgPatches {
         units[] = {};
         weapons[] = {};
         requiredVersion = REQUIRED_VERSION;
-        requiredAddons[] = {"A3AE_core"};
+        requiredAddons[] = {QDOUBLES(PREFIX,core)};
         author = AUTHOR;
         authors[] = { AUTHORS };
         authorUrl = "";
